d1af8cc086a1ba2ea786c48c8242b77fcbacd3e8
Negha-vm <neghamichael@gmail.com>
2019-04-01 19:56:20 +0530

    commit

---
+++ greatest.sh
@@ -0,0 +1,18 @@
+echo "enter three number"
+read a b c
+if [ $a -gt $b ]
+then
+if [ $a -gt $c ]
+then
+echo "$a is greater"
+else
+echo "$c is greater"
+fi
+else
+if [ $b -gt $c ]
+then
+echo "$b is greater"
+else
+echo "$c is greater"
+fi
+fi
